0e5f96f2368ed03e60dde2970394d5ef1759c81c
Enky <enkh6at@yahoo.com>
2023-01-27 12:51:58 +0000

    finish

--- src/router.js
+++ src/router.js
@@ -1,25 +1,40 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require("express");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 const router = express.Router();
+const secret = process.env.JWT_SECRET;
+class WrongBodyCredentials extends Error {
+  statusCode = 400;
+}
 
 const mockUser = {
-    username: 'authguy',
-    password: 'mypassword',
-    profile: {
-        firstName: 'Chris',
-        lastName: 'Wolstenholme',
-        age: 43
-    }
+  username: "authguy",
+  password: "mypassword",
+  profile: {
+    firstName: "Chris",
+    lastName: "Wolstenholme",
+    age: 43,
+  },
 };
 
-router.post('/login', (req, res) => {
-
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username !== mockUser.username || password !== mockUser.password) {
+    throw new WrongBodyCredentials("Invalid username or password");
+  }
+  const token = jwt.sign({ username: "authguy" }, secret);
+  res.json(token);
 });
 
-router.get('/profile', (req, res) => {
-  
+router.get("/profile", (req, res) => {
+  try {
+    const auth = req.get("Authorization");
+    jwt.verify(auth, secret);
+    return res.json({ profile: mockUser.profile });
+  } catch (e) {
+    res.status(498).json({ message: "Invalid token" });
+  }
 });
-
 
 module.exports = router;
